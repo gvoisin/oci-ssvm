05fc14f6c793484b8485faa263b8e9a60e1b7f2a
gvoisin <guenael.voisin@oracle.com>
2022-08-30 11:48:50 +0200

    POC Draft with Start action and started some formatting ...

---
+++ app/constants.tsx
@@ -0,0 +1,13 @@
+export const enum InstanceLifecycleState {
+    CreatingImage = "CREATING_IMAGE",
+    Moving = "MOVING",
+    Provisioning = "PROVISIONING",
+    Running = "RUNNING",
+    Starting = "STARTING",
+    Stopped = "STOPPED",
+    Stopping = "STOPPING",
+    Terminated = "TERMINATED",
+    Terminating = "TERMINATING",
+    Suspended = "SUSPENDED",
+    Suspending = "SUSPENDING"
+  }
